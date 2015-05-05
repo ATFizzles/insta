bc78bf1b2f80c76b724cd1ba0b9b872e3e9f7d41
Axel Tanner <axeltanner38@gmail.com>
2015-05-05 15:07:26 -0700

    Milestone 1 added info file

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php  
+	//configuration for our php server
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
+
+//Make constants using define
+define('client_id', 'cce4674a5fa04a9e80f41f598e8ff71d');
+define('client_secret', '9d46a69b815945db83e101b3f477108a');
+define('redirectURI', 'http://localhostlearninginsta1238/index.php');
+define('ImageDirectory', 'pics/');
+?>
+
+
+
+
+<!--CLIENT ID	cce4674a5fa04a9e80f41f598e8ff71d
+CLIENT SECRET	9d46a69b815945db83e101b3f477108a
+WEBSITE URL	http://localhostlearninginsta1238/index.php
+REDIRECT URI	http://localhostlearninginsta1238/index.php-->
