713c3cc73624eae9a621cb168b1e1ee23f73359e
Axel Tanner <axeltanner38@gmail.com>
2015-05-05 15:08:31 -0700

    Milestone 2 added index.php and pics file

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //Make constants using define
+//basically constants
 define('client_id', 'cce4674a5fa04a9e80f41f598e8ff71d');
 define('client_secret', '9d46a69b815945db83e101b3f477108a');
 define('redirectURI', 'http://localhostlearninginsta1238/index.php');
